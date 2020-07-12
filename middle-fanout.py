4a49cd239545a7762539c0033280d1dc6eb6c6ac
Moshe Zadka <zadka.moshe@gmail.com>
2020-07-12 15:48:22 -0700

    temp

--- middle-fanout.py
+++ middle-fanout.py
@@ -4,12 +4,12 @@ import httpx
 
 CLIENT = httpx.Client()
 URL = 'http://localhost:8080/slow?' + '&'.join(f"slow={float(part)}" for part in os.environ.get("SLOW", "").split(",") if part)
+FANOUT = int(os.environ.get("FANOUT", "1"))
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    result = CLIENT.get(URL)
-    value = result.json()['value']
-    return f'Got {value}'
+    all_values = sum(CLIENT.get(URL).json()["value"] for x in range(FANOUT))
+    return f'Total {all_values}'
